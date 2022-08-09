26807404974be506b7a4fa3995b65aee83b1277a
SeulkiLee <tmfrl3120@gmail.com>
2022-08-10 01:35:48 +0900

    env 설정 및 git ignore 추가, redirect 및 rewite 셋팅.

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import NavBar from '../components/NavBar'
 import Seo from '../components/Seo'
 import styles from '../styles/Home.module.css'
 
-const API_KEY = '1e1fb74a557fd4da0031779509d4dc51';
+
 
 const Home: NextPage = () => {
   const [movies, setMovies] = useState([]);
@@ -14,21 +14,43 @@ const Home: NextPage = () => {
     (async () => {
       const { results } = await ( 
           await fetch(
-            `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+            `/api/movies`
             )
           ).json();
           setMovies(results);
       })();
   }, []);
   return (
-    <div>
+    <div className="container">
       <Seo title='Home' />
       {!movies && <h4>Loading movies...</h4>}
       {movies?.map(movie => (
-        <div key={movie.id}>
-          <h4>{movie.original_title}</h4>
+         <div className="movie" key={movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt='movie poster' />
+         <h4>{movie.original_title}</h4>
         </div>
       ))}
+       <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
     </div>
   )
 }
